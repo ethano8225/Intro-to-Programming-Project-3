5910fe6c122249adebdbff76ecd89edfa1dcd287
etan8225 <eoconnor8225@gmail.com>
2024-04-25 19:18:20 -0400

    start

---
+++ Grid.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from Pixel import Pixel
+import numpy as np
+import random, time
+
+
+class Grid:
+    
+
+
+### To complete
+
+
+
+
+
+
+#########################################################
+############# Main code #################################
+#########################################################
+
+  
+def main(): 
+        
+        ##### create a window, canvas 
+        root = Tk()                # instantiate a tkinter window
+        mesh = Grid(root,50,30,20) # instantiate a Grid object
+        mesh.random_pixels(25,1) # generate 25 random (white) pixels in the Grid
+
+        
+        ####### Tkinter binding mouse actions
+        root.bind("<Button-1>",lambda e:mesh.addxy(e.x,e.y))
+        root.bind("<Button-3>",lambda e:mesh.delxy(e.x,e.y))
+        
+
+        root.mainloop() # wait until the window is closed
+        
+
+if __name__=="__main__":
+    main()
+
