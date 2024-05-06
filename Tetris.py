0db2db397612844b047c2d4df2c997300f9160d2
etan8225 <eoconnor8225@gmail.com>
2024-05-06 16:33:31 -0400

    7

--- Tetris.py
+++ Tetris.py
@@ -16,21 +16,21 @@ import time
     
 def main():
     ##### create a window, canvas 
-        root = Tk() # instantiate a tkinter window
-        game=Tetris(root,36,12,25) 
+    root = Tk() # instantiate a tkinter window
+    game=Tetris(root,36,12,25) 
         
-        ####### Tkinter binding mouse actions
-        root.bind("<Up>",lambda e:game.up())
-        root.bind("<Left>",lambda e:game.left())
-        root.bind("<Right>",lambda e:game.right())
-        root.bind("<Down>",lambda e:game.down())
-        root.bind("<p>",lambda e:game.pause())        
+    ####### Tkinter binding mouse actions
+    root.bind("<Up>",lambda e:game.up())
+    root.bind("<Left>",lambda e:game.left())
+    root.bind("<Right>",lambda e:game.right())
+    root.bind("<Down>",lambda e:game.down())
+    root.bind("<p>",lambda e:game.pause())        
 
-        while True:
-            if not game.is_pause(): game.next()
-            root.update()   # update the graphic
-            time.sleep(0.25)  # wait few second (simulation)
-            if game.is_game_over(): break
+    while True:
+        if not game.is_pause(): game.next()
+        root.update()   # update the graphic
+        time.sleep(0.25)  # wait few second (simulation)
+        if game.is_game_over(): break
         
         root.mainloop() # wait until the window is closed
 
