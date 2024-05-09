d78b0214c578bf6ba55ba3fe186ec97c6c8042bf
etan8225 <eoconnor8225@gmail.com>
2024-05-08 20:59:24 -0400

    w

--- Grid.py
+++ Grid.py
@@ -8,40 +8,37 @@ class Grid:
     def __init__(self,root,nrow,ncol,scale):
         self.root=root
         self.nrow=nrow
-        self.ncol=ncol
+        self.ncol=ncol          #set default values
         self.scale=scale
         self.canvas=Canvas(root,width=ncol*scale,height=nrow*scale, bg='white')
         self.canvas.pack()
         self.Integer= np.zeros((self.nrow,self.ncol),dtype=np.int8)
         self.pixelist=[]
         for row in range(nrow):
-            for col in range(ncol):
+            for col in range(ncol):     
                 self.pixelist.append(Pixel(self.canvas,row,col,self.nrow,self.ncol,self.scale,0))
         
     def random_pixels(self,num,color_id):
         for k in range(num):
             i = random.randint(0,self.nrow-1)
-            j=random.randint(0,self.ncol-1)
+            j=random.randint(0,self.ncol-1)     #add random pixels to the screen using random (imported)
             self.add_ij(i,j,color_id)
 
     def addxy(self,x,y):
         print(f"insert {x} {y} {y//self.scale} {x//self.scale} {self.Integer[y//self.scale,x//self.scale]}")
-        self.add_ij(y//self.scale,x//self.scale,1)
+        self.add_ij(y//self.scale,x//self.scale,1)      #adds the point to the graph
 
     def add_ij(self,i,j,c):
-        if c>0:
+        if c>0:                 #actually adds the point here
             self.pixelist.append(Pixel(self.canvas,i,j,self.nrow,self.ncol,self.scale,c))
-            #if self.nrow==20:
-            #    if i > 19:
-            #        i = 19
             self.Integer[i,j] = c
 
-    def delxy(self,x,y):
+    def delxy(self,x,y):            #delete the point from x,y
         print(f"delete {x} {y} {y//self.scale} {x//self.scale} {self.Integer[y//self.scale,x//self.scale]}")
         self.del_ij(y//self.scale,x//self.scale)
 
     def del_ij(self,i,j):
-        if self.Integer[i,j] != 0:
+        if self.Integer[i,j] != 0:      #actually make the change on grid
             self.Integer[i,j] = 0
             self.reset()
         else:
@@ -51,7 +48,7 @@ class Grid:
         for pixel in self.pixelist:
                 pixel.delete()
         for row in range(self.nrow):
-            for col in range(self.ncol):
+            for col in range(self.ncol):        #reset the grid,
                 if self.Integer[row, col] > 0:
                     self.add_ij(row,col,self.Integer[row,col])
                 else:
@@ -64,19 +61,19 @@ class Grid:
                 pixels.append(Pixel(self.canvas,i,0,self.nrow,self.ncol,self.scale,7,[0,1]))
                 pixels.append(Pixel(self.canvas,i,1,self.nrow,self.ncol,self.scale,7,[0,1]))
                 pixels.append(Pixel(self.canvas,i,2,self.nrow,self.ncol,self.scale,7,[0,1]))
-                
+                    #adds the points to the left and right
                 pixels.append(Pixel(self.canvas,i,self.ncol,self.nrow,self.ncol,self.scale,7,[0,-1]))
                 pixels.append(Pixel(self.canvas,i,self.ncol-1,self.nrow,self.ncol,self.scale,7,[0,-1]))
                 pixels.append(Pixel(self.canvas,i,self.ncol-2,self.nrow,self.ncol,self.scale,7,[0,-1]))
                 
                 for rowflush in range(self.ncol):
                         for p in pixels:
-                                p.next()
+                                p.next()        #this moves the purple lines for animation
                         self.root.update()
                         time.sleep(0.01)
                 self.Integer[1:i+1,:]=self.Integer[0:i,:]
                 self.Integer[0,:] = 0
-                self.reset()
+                self.reset()            #resets the row
 
 #########################################################
 ############# Main code #################################
--- Pixel.py
+++ Pixel.py
@@ -7,69 +7,69 @@ class Pixel:
     
     ### to complete        
     
-    def __init__(self,canvas,i,j,nrow,ncol,scale=20,colorid=1,vector=[0,0]) -> None:
-        color=['black','white','yellow','red','blue','green','orange','purple','brown','cyan']
+    def __init__(self,canvas,i,j,nrow,ncol,scale=20,colorid=1,vector=[0,0]) -> None: # Constructor method for Pixel objects
+        color=['black','white','yellow','red','blue','green','orange','purple','brown','cyan'] #list of colors
         self.canvas=canvas
         self.i=i
         self.j=j
-        self.nrow=nrow
+        self.nrow=nrow              #Assigning variables to instance variables
         self.ncol=ncol
         self.scale=scale
         self.clr=color[colorid]
         self.vector=vector
         if self.i > self.nrow:
-            self.i=self.i%self.nrow
+            self.i=self.i%self.nrow #Adjusting i to fit within the number of rows
         if self.j % self.ncol:
-            self.j=self.j%self.ncol
-        self.pix = canvas.create_rectangle(self.j*self.scale,self.i*self.scale,self.j*self.scale+self.scale,self.i*self.scale+self.scale,fill=str(self.clr),outline='black')
+            self.j=self.j%self.ncol #Adjusting j to fit within the number of columns
+        self.pix = canvas.create_rectangle(self.j*self.scale,self.i*self.scale,self.j*self.scale+self.scale,self.i*self.scale+self.scale,fill=str(self.clr),outline='black') #Creating a rectangle on the canvas representing the pixel
         if self.clr == "black":
-            self.pix = canvas.create_rectangle(self.j*self.scale,self.i*self.scale,self.j*self.scale+self.scale,self.i*self.scale+self.scale,fill=str(self.clr),outline='gray')
-    def right(self):
+            self.pix = canvas.create_rectangle(self.j*self.scale,self.i*self.scale,self.j*self.scale+self.scale,self.i*self.scale+self.scale,fill=str(self.clr),outline='gray')#If a square is black, the square's outline is set to gray
+    def right(self): # Method to move right
         self.vector=[0,1]
         
-    def left(self):
+    def left(self): # Method to move left
         self.vector=[0,-1]  
         
-    def down(self):
+    def down(self): # Method to move down
         self.vector=[1,0]  
         
-    def up(self):
+    def up(self): # Method to move up
         self.vector=[-1,0]
         
     def next(self):
-        if self.vector == [-1,0]: #up
+        if self.vector == [-1,0]: #if up
             if self.i < 1:
-                self.canvas.coords(self.pix, self.j*self.scale, self.nrow*self.scale-self.scale, self.j*self.scale+self.scale, self.nrow*self.scale)
+                self.canvas.coords(self.pix, self.j*self.scale, self.nrow*self.scale-self.scale, self.j*self.scale+self.scale, self.nrow*self.scale) #If Pixel is at top edge, move to bottom edge
                 self.i = self.nrow - 1
             else:    
                 self.canvas.move(self.pix,0,-self.scale)
                 self.i = self.i - 1
-        if self.vector == [1,0]: #down
+        if self.vector == [1,0]: #if down
             if self.i > self.nrow-2:
-                self.canvas.coords(self.pix, self.j*self.scale, 0, self.j*self.scale+self.scale, self.scale)
+                self.canvas.coords(self.pix, self.j*self.scale, 0, self.j*self.scale+self.scale, self.scale) #If Pixel is at bottom edge, move to top edge
                 self.i = 0
             else:    
                 self.canvas.move(self.pix,0,self.scale)
                 self.i += 1
-        if self.vector == [0,-1]: #left
+        if self.vector == [0,-1]: #if left
             if self.j < 1:
-                self.canvas.coords(self.pix, self.ncol*self.scale-self.scale, self.i*self.scale, self.ncol*self.scale, self.i*self.scale+self.scale)
+                self.canvas.coords(self.pix, self.ncol*self.scale-self.scale, self.i*self.scale, self.ncol*self.scale, self.i*self.scale+self.scale)#If Pixel is at left edge, move to right edge
                 self.j = self.ncol - 1
             else:
                 self.canvas.move(self.pix,-self.scale,0)
                 self.j = self.j - 1
-        if self.vector == [0,1]: #right
+        if self.vector == [0,1]: #if right
             if self.j > self.ncol - 2:
-                self.canvas.coords(self.pix, 0, self.i*self.scale, self.scale, self.i*self.scale+self.scale)
+                self.canvas.coords(self.pix, 0, self.i*self.scale, self.scale, self.i*self.scale+self.scale) #If Pixel is at right edge, move to left edge
                 self.j = 0
             else:
                 self.canvas.move(self.pix,self.scale,0)
                 self.j += 1
 
-    def delete(self):
+    def delete(self):#Deletes the pixel
         self.canvas.delete(self.pix)
 
-    def __str__(self):
+    def __str__(self): #Returns a string representation of the pixel's position and color
         return "(%s,%s) %s"%(self.i,self.j,self.clr)
 
 
@@ -179,8 +179,6 @@ def test5(root,canvas,nrow,ncol,scale):
         time.sleep(0.05)  # wait in second (simulation)
 
 
-
-        
 
 ###################################################
 #################### Main method ##################
